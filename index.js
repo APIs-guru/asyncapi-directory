da30e4f021b926817cbaf58b7081569edba6a3ae
Mike Ralphson <mike.ralphson@gmail.com>
2018-11-20 22:51:48 +0000

    Prep for travis script

---
+++ index.js
@@ -0,0 +1,14 @@
+#!/usr/bin/env node
+
+'use strict';
+
+const fs = require('fs');
+
+const liquid = require('liquid-node');
+const yaml = require('js-yaml');
+
+console.log('Starting...');
+
+//
+
+console.log('Finishing...');
